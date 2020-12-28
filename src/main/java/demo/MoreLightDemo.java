3539bcedfc5e1e1d90f2730eed7fa8e9a31dc994
gq <575608952@qq.com>
2020-12-28 21:42:31 +0800

    开始MoreLightDemo

---
+++ src/main/java/demo/MoreLightDemo.java
@@ -0,0 +1,9 @@
+package demo;
+
+/**
+ * @Author Gq
+ * @Date 2020/12/28 21:36
+ * @Version 1.0
+ **/
+public class MoreLightDemo {
+}
---
+++ src/main/java/graphic/DirectionalLight.java
@@ -0,0 +1,49 @@
+package graphic;
+
+import org.joml.Vector3f;
+
+/**
+ * @Author Gq
+ * @Date 2020/12/28 21:37
+ * @Version 1.0
+ **/
+public class DirectionalLight {
+
+    private Vector3f colour;
+    private Vector3f direction;
+    private float intensity;
+
+    public DirectionalLight(Vector3f colour, Vector3f direction, float intensity) {
+        this.colour = colour;
+        this.direction = direction;
+        this.intensity = intensity;
+    }
+
+    public DirectionalLight(DirectionalLight directionalLight) {
+        this(new Vector3f(directionalLight.colour), new Vector3f(directionalLight.direction), directionalLight.intensity);
+    }
+
+    public Vector3f getColour() {
+        return colour;
+    }
+
+    public void setColour(Vector3f colour) {
+        this.colour = colour;
+    }
+
+    public Vector3f getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Vector3f direction) {
+        this.direction = direction;
+    }
+
+    public float getIntensity() {
+        return intensity;
+    }
+
+    public void setIntensity(float intensity) {
+        this.intensity = intensity;
+    }
+}
